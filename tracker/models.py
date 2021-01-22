611b45ed3df08bc5f1a8eb69104c2a3f07626d48
cakeinsauce <minefrs@gmail.com>
2021-01-22 13:47:28 +0300

    Add Severity, IssuePriority, IssueType and IssueStatus classes with fields

--- tracker/models.py
+++ tracker/models.py
@@ -6,9 +6,9 @@ from django.utils import timezone
 
 class UserRole(models.Model):
     ADMIN = 'ADMINISTRATOR'  # can do everything
-    MODER = 'MODERATOR'  # like admin but less
-    USER = 'USER'  # authorized user
-    VIEWER = 'VIEWER'  # not authorized user
+    MODER = 'MODERATOR'      # like admin but less
+    USER = 'USER'            # authorized user
+    VIEWER = 'VIEWER'        # not authorized user
 
     USER_CHOICES = (
         (ADMIN, 'Administrator'),
@@ -23,6 +23,107 @@ class UserRole(models.Model):
 
     def __str__(self):
         return self.user_role
+
+
+class Severity(models.Model):
+    BLOCK = 'BLOCKER'  # cause of product failure
+    CRIT = 'CRITICAL'  # cause of partial product failure
+    MAJOR = 'MAJOR'    # some of the main logic works incorrectly
+    MINOR = 'MINOR'    # doesn't break logic
+    TRIV = 'TRIVIAL'   # doesn't affect the overall quality of product
+
+    SEVERITY_CHOICES = (
+        (BLOCK, 'Blocker'),
+        (CRIT, 'Critical'),
+        (MAJOR, 'Major'),
+        (MINOR, 'Minor'),
+        (TRIV, 'Trivial'),
+    )
+
+    issue_severity = models.CharField(max_length=9,
+                                choices=SEVERITY_CHOICES,
+                                null=True,
+                                default=TRIV)
+
+    def __str__(self):
+        return self.issue_severity
+
+
+class IssuePriority(models.Model):
+    HIGHEST = 'HIGHEST'  # will block progress
+    HIGH = 'HIGH'        # could block progress
+    MEDIUM = 'MEDIUM'    # potential to affect progress
+    LOW = 'LOW'          # easily worked around
+    LOWEST = 'LOWEST'    # little or no impact on progress
+
+    PRIORITY_CHOICES = (
+        (HIGHEST, 'Highest'),
+        (HIGH, 'High'),
+        (MEDIUM, 'Medium'),
+        (LOW, 'Low'),
+        (LOWEST, 'Lowest'),
+    )
+
+    issue_rank = models.CharField(max_length=8,
+                                  choices=PRIORITY_CHOICES,
+                                  default=MEDIUM)
+
+    def __str__(self):
+        return self.issue_rank
+
+
+class IssueType(models.Model):
+    BUG = 'BUG'
+    EPIC = 'EPIC'
+    IMPROVE = 'IMPROVEMENT'
+    FEATURE = 'FEATURE'
+    TASK = 'TASK'
+    SUBTASK = 'SUBTASK'
+
+    TYPE_CHOICES = (
+        (BUG, 'Bug'),
+        (EPIC, 'Epic'),
+        (IMPROVE, 'Improvement'),
+        (FEATURE, 'New Feature'),
+        (TASK, 'Task'),
+        (SUBTASK, 'Sub-task'),
+    )
+
+    issue_type = models.CharField(max_length=12,
+                                  choices=TYPE_CHOICES,
+                                  default=TASK)
+
+    def __str__(self):
+        return self.issue_type
+
+
+class IssueStatus(models.Model):
+    OPEN = 'OPEN'
+    PROGRESS = 'PROGRESS'
+    DONE = 'DONE'
+    TODO = 'TODO'
+    REVIEW = 'REVIEW'
+    APPROVED = 'APPROVED'
+    REJECTED = 'REJECTED'
+    CANCELLED = 'CANCELLED'
+
+    STATUS_CHOICES = (
+        (OPEN, 'Open'),
+        (PROGRESS, 'In Progress'),
+        (DONE, 'Done'),
+        (TODO, 'To Do'),
+        (REVIEW, 'In Review'),
+        (APPROVED, 'Approved'),
+        (REJECTED, 'Rejected'),
+        (CANCELLED, 'Cancelled'),
+    )
+
+    issue_status = models.CharField(max_length=10,
+                                    choices=STATUS_CHOICES,
+                                    default=TODO)
+
+    def __str__(self):
+        return self.issue_status
 
 
 class UserProfile(models.Model):
